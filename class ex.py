0e826e3ce1163e72d90fa73b38c5e28a32140143
fatima <fatimadzhumaz2005@gmail.com>
2025-02-13 17:24:21 +0600

    initial commit

---
+++ class ex.py
@@ -0,0 +1,26 @@
+# numbers = [12,7,34,23,45,66,89,90,41,55]
+# num1 =[]
+# num2 =[]
+# for num in numbers:
+#     if num % 2 ==0:
+#         num1.append(num)
+#     else:
+        
+#         num2.append(num)
+# print(num1)
+# print(num2)
+
+
+# numbers = [22,45,67,12,89,34,55,90,10]
+# min_number1 =0
+# max_number2 =0
+
+# for i in numbers:
+#     if i < min_number1:
+#         min_number1 = i
+#     if i > max_number2:
+#         max_number2 = i
+     
+# print(min_number1)
+# print(max_number2)
+
